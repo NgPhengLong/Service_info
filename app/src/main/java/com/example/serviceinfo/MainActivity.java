e1d1e2a3b33dc0c95282ab7fb4e42589a68ee8ec
NgPhengLong <ngpl-wm19@student.tarc.edu.my>
2020-07-23 14:16:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/serviceinfo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.serviceinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
